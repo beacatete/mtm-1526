c2cd57988f6af78b589c01726b4636bddcfb3555
beacatete <169159901+beacatete@users.noreply.github.com>
2024-05-21 12:16:27 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,46 @@
+//Declare three variables, a and b and c
+let a = 3;
+let b = 5;
+let c;
+
+
+//TASK 1
+let expressions = 
+
+    `let a = 3
+    let b = 5
+    let c;
+    -----------------
+    a + b =  ${a + b}
+    a - b = ${a - b}
+    a * b = ${a * b}
+    a / b =  ${a / b}
+    a % b = ${a % b}
+    a += b = ${a += b}
+    a -= b = ${a -= b}
+    a *= b = ${a *= b}
+    a /= b = ${a /= b}
+    a %= b = ${a %= b}
+    a == b: ${a == b}
+    a != b = ${a != b}
+    a > b = ${a > b}
+    a < b = ${a < b}
+    !a && !c = ${!a && !c}
+    !a || !c = ${!a || !c}` 
+;
+
+//to print
+console.log(expressions);
+
+
+//TASK 2
+
+let first_name = "Beatriz";
+let last_name = "Catete";
+let email = "cate0023@algonquinlive.com";
+
+let sentence = `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+
+//to print
+console.log(sentence);
+
